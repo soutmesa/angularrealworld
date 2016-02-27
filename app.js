f9b7430c351e8792e095f396558a688bb5bc9778
Mesa <mesa@macbookpromama.edmcambodia.org>
2016-02-27 12:43:58 +0700

    update routing

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 angular.module('myFirstApp', ['ngRoute'])
 .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
-	$routeProvider.when('/', {
-		templateUrl: 'index.html'
-	}).when('/about', {
+	$routeProvider.when('/about', {
 		templateUrl: 'views/about.html'
 	}).when('/contact', {
 		templateUrl: 'views/contact.html'
-	}).otherwise({templateUrl: 'views/404.html'})
+	}).otherwise({templateUrl: 'index.html'})
 	// $locationProvider.html5Mode({
 	// 	enabled: true,
  //  		requireBase: false
