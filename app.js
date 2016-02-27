88657f863b6699496f10ce966a497aeb191797ee
Mesa <mesa@macbookpromama.edmcambodia.org>
2016-02-27 15:10:27 +0700

    Using Route with Controller

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 angular.module('myFirstApp', ['ngRoute'])
 .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
-	$routeProvider.when('/about', {
-		templateUrl: 'views/about.html'
-	}).when('/contact', {
-		templateUrl: 'views/contact.html'
+	$routeProvider.when('/about/:param', {
+		templateUrl: 'views/about.html',
+		controller: 'AboutCtrl'
+	}).when('/contact/:param1', {
+		templateUrl: 'views/contact.html',
+		controller: 'ContactCtrl'
 	}).otherwise({templateUrl: '404.html'})
 }])
 .factory('personService', function(){
--- controller.js
+++ controller.js
@@ -1,7 +1,14 @@
-angular.module('myFirstApp').controller('myController', function($scope, personService){
+angular.module('myFirstApp')
+.controller('myController', function($scope, personService){
 	$scope.firstName = 'Trainee';
 	$scope.lastName = 'Russo';
 	$scope.printName = function(){
 		return personService.printName($scope.firstName, $scope.lastName);
 	}
 })
+.controller('AboutCtrl', ['$scope', '$routeParams', function($scope, $routeParams){
+	$scope.input = $routeParams.param;
+}])
+.controller('ContactCtrl', ['$scope', '$routeParams', function($scope, $routeParams){
+	$scope.input = $routeParams.param1;
+}])
