5c9b71c0a802d97e998b1ee41b42a2212e9e906a
Mesa <mesa@macbookpromama.edmcambodia.org>
2016-02-27 14:46:53 +0700

    update routing

--- app.js
+++ app.js
@@ -4,11 +4,7 @@ angular.module('myFirstApp', ['ngRoute'])
 		templateUrl: 'views/about.html'
 	}).when('/contact', {
 		templateUrl: 'views/contact.html'
-	}).otherwise({templateUrl: 'index.html'})
-	// $locationProvider.html5Mode({
-	// 	enabled: true,
- //  		requireBase: false
-	// });
+	}).otherwise({templateUrl: '404.html'})
 }])
 .factory('personService', function(){
 	var person = {};
